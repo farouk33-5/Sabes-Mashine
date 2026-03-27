44a3f58670a82c3d247b85d745b8aac235d5e7a2
farouk33-5 <126901490+farouk33-5@users.noreply.github.com>
2026-03-27 20:28:50 +0100

    Add Home component with SalesMachine integration

---
+++ page.jsx
@@ -0,0 +1,10 @@
+import SalesMachine from './sales-machine';
+
+export default function Home() {
+  return (
+    <div className="bg-black min-h-screen">
+      <SalesMachine />
+    </div>
+  );
+}
+
